0ee50e2ac6cd99ee65acc785c83bd86339bcd81f
lililen <165857442+lililen@users.noreply.github.com>
2026-02-24 23:59:47 -0800

    summary of staments+ pie chart

---
+++ pdfjs-shims.d.ts
@@ -0,0 +1,4 @@
+declare module "pdfjs-dist/build/pdf.worker.min.js";
+declare module "pdfjs-dist/build/pdf.worker.min.mjs";
+declare module "pdfjs-dist/build/pdf.worker.min.mjs";
+declare module "pdfjs-dist/build/pdf";
